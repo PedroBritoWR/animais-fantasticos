db445c29e35ac98d4e9e39ca1806792b20d0ad4b
PedroBritoWR <pedrowpvh@hotmail.com>
2024-06-03 13:49:44 -0400

    scroll suave

--- scripts/script3.js
+++ scripts/script3.js
@@ -36,3 +36,18 @@ tabMenu.forEach((itemMenu, index) => {
 }
 };
   initAccordion();
+   
+  function scrollToSection (event) {
+    event.preventDefault(); 
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    const topo = section.offsetTop;
+    window.scrollTo({
+      top: topo,
+      behavior: 'smooth'
+    })
+  }
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
