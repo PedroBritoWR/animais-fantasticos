e8528e4f77dfebafa830f16a2a7f4bcff9603c27
PedroBritoWR <pedrowpvh@hotmail.com>
2024-06-05 13:51:52 -0400

    finalizado

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,82 @@
-carregarPosts = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const posts = await response.json();
-  telaDePosts(posts);
-}
+//NAVEGAÇÃO POR TAB
+function initTabNav() {
+  const tabMenu = document.querySelectorAll('.js-tabmenu li');
+  const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-telaDePosts = (posts) => {
-  const xd = posts.slice(0, 5);
-  const container = document.querySelector('.container');
-  xd.forEach((post) => {
-    const postArea = document.createElement('div');
-    postArea.classList.add('post');
-    postArea.innerHTML = `
-   <h3>${post.title}</h3>
-   <p>${post.body}</p>
-   `;
-    container.appendChild(postArea);
+  if (tabMenu.length && tabContent.length) {
+
+    tabContent[0].classList.add('ativo');
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove('ativo');
+      })
+      tabContent[index].classList.add('ativo');
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
+        activeTab(index);
+      });
+    })
+  };
+};
+
+initTabNav();
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const activeClass = 'ativo';
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
+    });
+  }
+};
+initAccordion();
+
+//ANIMAÇÃO SCROLL SUAVE
+function initScrollSuave() {
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    const topo = section.offsetTop;
+    window.scrollTo({
+      top: topo,
+      behavior: 'smooth'
+    })
+  }
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
   });
 }
-window.onload = () => {
-  carregarPosts();
+initScrollSuave();
+
+// ANIMAÇÃO DAS SECTIONS DE CONTEUDO
+function initAnimacaoScroll() {
+const sections = document.querySelectorAll('.js-scroll');
+const windowMetade = window.innerHeight * 0.3;
+function animasScroll() {
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top - windowMetade;
+    const isSectionVisible = (sectionTop - windowMetade) < 0;
+    console.log(sectionTop);
+    if (isSectionVisible) {
+      section.classList.add('ativo');
+    } else {
+      section.classList.remove('ativo');
+    }
+  })
 }
-
-//titulo com evento ao ser clicado com mensagem no console
-const titulo = document.querySelector('h1');
-titulo.addEventListener('click', function () {
-  console.log('clicou no titulo');
-  consultarUrlDaPagina();
-});
-
-//consulta da url da pagina atual
-function consultarUrlDaPagina() {
-  const url = window.location.href;
-  console.log(url);
+animasScroll();
+window.addEventListener('scroll', animasScroll);
 }
-//pegando o primeiro elemento com a classe ativada nomeada de title no meu DOM
-const activeElement = document.querySelectorAll('.title');
-const firstElement = activeElement[0];
-console.log(firstElement);
-
-//consulta a linguagem do navegador
-const language = navigator.language;
-console.log(language);
-
-//retornando a largura da janela
-const windowWidth = window.innerWidth;
-console.log(windowWidth);
-
-
-
-
-
-
-
+initAnimacaoScroll();
